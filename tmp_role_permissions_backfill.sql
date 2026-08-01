4c4344dbff6020c9b73654437ffe54be5bf327d5
Mario <longbird@gmail.com>
2026-08-01 23:45:30 +0900

    chore: stop tracking generated artifacts

--- tmp_role_permissions_backfill.sql
+++
@@ -1,8 +0,0 @@
-UPDATE "rolePermissions"
-SET
-  "canView" = "canAccess",
-  "canCreate" = "canAccess",
-  "canUpdate" = "canAccess",
-  "canDelete" = "canAccess",
-  "canOperate" = "canAccess",
-  "canExport" = CASE WHEN "menuKey" LIKE 'reports/%' THEN "canAccess" ELSE FALSE END;
